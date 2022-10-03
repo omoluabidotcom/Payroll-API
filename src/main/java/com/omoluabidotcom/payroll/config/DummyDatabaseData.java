b2f9f0fe860ba4b3d9d8f81bc9d3ae1ec65511d0
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-03 12:20:27 +0100

    dummy data add to database

--- src/main/java/com/omoluabidotcom/payroll/config/DummyDatabaseData.java
+++ src/main/java/com/omoluabidotcom/payroll/config/DummyDatabaseData.java
@@ -1,2 +1,28 @@
-package com.omoluabidotcom.payroll.config;public class DummyDatabaseData {
+package com.omoluabidotcom.payroll.config;
+
+import com.omoluabidotcom.payroll.entity.Employee;
+import com.omoluabidotcom.payroll.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DummyDatabaseData {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(DummyDatabaseData.class);
+
+    @Bean
+    CommandLineRunner commandLineRunner(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + employeeRepository.save(new Employee("Goodluck Ebele", "Kind")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Umar Yaradua", "Gentle")));
+        };
+    }
 }
