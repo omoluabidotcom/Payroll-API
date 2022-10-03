a9a822c8813bb3beebe52b43a8dc539b6ad03088
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-03 23:06:35 +0100

    employeecontroller add

---
+++ src/main/java/com/omoluabidotcom/payroll/Controller/EmployeeController.java
@@ -0,0 +1,68 @@
+package com.omoluabidotcom.payroll.Controller;
+
+import java.util.List;
+
+import com.omoluabidotcom.payroll.entity.Employee;
+import com.omoluabidotcom.payroll.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+//    EmployeeController(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+
+    // Aggregate root
+
+    // tag::get-aggregate-root[]
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return employeeRepository.findAll();
+    }
+    // end::get-aggregate-root[]
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return employeeRepository.save(newEmployee);
+    }
+
+    // Single item
+
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return employeeRepository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+        return employeeRepository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return employeeRepository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return employeeRepository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
