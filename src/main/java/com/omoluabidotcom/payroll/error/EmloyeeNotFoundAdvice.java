7209c79f8a31e23da1b7b4f1a1c1bcaddaa7d0e3
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-06 22:43:23 +0100

    exception handling for employee not found add

---
+++ src/main/java/com/omoluabidotcom/payroll/error/EmloyeeNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package com.omoluabidotcom.payroll.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EmloyeeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String EmployeeNotFoundHandler(EmployeeNotFoundException ex) {
+
+        return ex.getMessage();
+    }
+}
