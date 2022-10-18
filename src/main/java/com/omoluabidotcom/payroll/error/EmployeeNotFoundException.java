0c0cbdac3f81f5b05e559f4efdcc29329aab8db5
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-18 01:15:38 +0100

    Syntax refactor in exception class constructor

--- src/main/java/com/omoluabidotcom/payroll/error/EmployeeNotFoundException.java
+++ src/main/java/com/omoluabidotcom/payroll/error/EmployeeNotFoundException.java
@@ -4,6 +4,6 @@ public class EmployeeNotFoundException extends RuntimeException{
 
     public EmployeeNotFoundException(Long id) {
 
-        super("Could not find employee " + id);
+        super("Could not find employee with " + id);
     }
 }
