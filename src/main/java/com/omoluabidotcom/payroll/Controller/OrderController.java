8be4617274c443d13191ffaa16be6e545ca54595
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-25 23:08:12 +0100

    ordercontroller class add

--- src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
+++ src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
@@ -1,2 +1,9 @@
-package com.omoluabidotcom.payroll.Controller;public class OrderController {
+package com.omoluabidotcom.payroll.Controller;
+
+import com.omoluabidotcom.payroll.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
 }
