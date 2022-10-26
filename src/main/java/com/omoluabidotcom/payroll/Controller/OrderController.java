df52d8fce6d4963ea5b401506fb0d06d799b202e
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-26 23:12:46 +0100

    orderrepository and ordermodelassembler autowired into ordercontroller constructor

--- src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
+++ src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
@@ -1,9 +1,23 @@
 package com.omoluabidotcom.payroll.Controller;
 
+import com.omoluabidotcom.payroll.model.OrderModelAssembler;
 import com.omoluabidotcom.payroll.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    private OrderModelAssembler orderModelAssembler;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository,
+                           OrderModelAssembler orderModelAssembler)
+    {
+        this.orderRepository = orderRepository;
+        this.orderModelAssembler = orderModelAssembler;
+    }
+
 }
