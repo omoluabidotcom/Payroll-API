f69e19ebf690a81a8a720d59d8ffb3598c19ad69
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-26 23:23:43 +0100

    all method to return all orders add

--- src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
+++ src/main/java/com/omoluabidotcom/payroll/Controller/OrderController.java
@@ -1,9 +1,19 @@
 package com.omoluabidotcom.payroll.Controller;
 
+import com.omoluabidotcom.payroll.entity.Order;
 import com.omoluabidotcom.payroll.model.OrderModelAssembler;
 import com.omoluabidotcom.payroll.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class OrderController {
@@ -20,4 +30,13 @@ public class OrderController {
         this.orderModelAssembler = orderModelAssembler;
     }
 
+    @GetMapping("/orders")
+    public CollectionModel<EntityModel<Order>> all() {
+        List<EntityModel<Order>> order = orderRepository.findAll().stream()
+                .map(orderModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(order,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
 }
