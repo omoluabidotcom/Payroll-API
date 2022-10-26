a96cded9a35d2f354fb9778745795f268af6ee03
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-26 23:56:45 +0100

    employee not found exception class error syntax adjust

--- src/main/java/com/omoluabidotcom/payroll/error/EmployeeNotFoundException.java
+++ src/main/java/com/omoluabidotcom/payroll/error/EmployeeNotFoundException.java
@@ -4,6 +4,6 @@ public class EmployeeNotFoundException extends RuntimeException{
 
     public EmployeeNotFoundException(Long id) {
 
-        super("Could not find employee with id " + id);
+        super("Employee with the id " + id + " does not exist");
     }
 }
