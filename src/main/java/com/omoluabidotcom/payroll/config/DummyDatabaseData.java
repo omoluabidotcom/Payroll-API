5c8b0f57a373cca203077e1248d2a69ab1b2c138
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-30 11:50:21 +0100

    dummydatabase loader adjust to load in order entity

--- src/main/java/com/omoluabidotcom/payroll/config/DummyDatabaseData.java
+++ src/main/java/com/omoluabidotcom/payroll/config/DummyDatabaseData.java
@@ -1,7 +1,10 @@
 package com.omoluabidotcom.payroll.config;
 
 import com.omoluabidotcom.payroll.entity.Employee;
+import com.omoluabidotcom.payroll.entity.Order;
 import com.omoluabidotcom.payroll.repository.EmployeeRepository;
+import com.omoluabidotcom.payroll.repository.OrderRepository;
+import com.omoluabidotcom.payroll.utility.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,26 @@ public class DummyDatabaseData {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     private static final Logger log = LoggerFactory.getLogger(DummyDatabaseData.class);
 
     @Bean
-    CommandLineRunner commandLineRunner(EmployeeRepository repository) {
+    CommandLineRunner commandLineRunner() {
 
         return args -> {
-            log.info("Preloading " + employeeRepository.save(new Employee("Goodluck", "Ebele", "Kind")));
-            log.info("Preloading " + employeeRepository.save(new Employee("Umar", "Yaradua", "Gentle")));
+            employeeRepository.save(new Employee("Goodluck", "Ebele", "Kind"));
+            employeeRepository.save(new Employee("Umar", "Yaradua", "Gentle"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloading " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("Samsung", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preloading " + order));
+
         };
     }
+
 }
