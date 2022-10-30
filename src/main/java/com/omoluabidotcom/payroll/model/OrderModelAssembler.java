3869816c39f55d2634793e8a5250484114a7fba7
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-30 12:28:25 +0100

    ordermodelassembler adjust for order entity

--- src/main/java/com/omoluabidotcom/payroll/model/OrderModelAssembler.java
+++ src/main/java/com/omoluabidotcom/payroll/model/OrderModelAssembler.java
@@ -3,6 +3,7 @@ package com.omoluabidotcom.payroll.model;
 import com.omoluabidotcom.payroll.Controller.EmployeeController;
 import com.omoluabidotcom.payroll.Controller.OrderController;
 import com.omoluabidotcom.payroll.entity.Order;
+import com.omoluabidotcom.payroll.utility.Status;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -15,8 +16,16 @@ public class OrderModelAssembler implements RepresentationModelAssembler<Order, 
 
     @Override
     public EntityModel<Order> toModel(Order order) {
-        return EntityModel.of(order,
+        EntityModel<Order> orderModel = EntityModel.of(order,
                 linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
                 linkTo(methodOn(OrderController.class).all()).withRel("orders"));
+
+        if(order.getStatus() == Status.IN_PROGRESS) {
+            orderModel.add(linkTo(methodOn(OrderController.class).cancelOrder(order.getId())).withRel("cancel"));
+
+            orderModel.add(linkTo(methodOn(OrderController.class).completeOrder(order.getId())).withRel("comlete"));
+        }
+
+        return orderModel;
     }
 }
